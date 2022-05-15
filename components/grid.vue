cc5a798fd3496c143e23fb786946b65e951c738b
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-05-15 04:04:09 +0300

    refactor grid template

--- components/grid.vue
+++ components/grid.vue
@@ -10,16 +10,7 @@
             <div class="bar vertical vertical-2"></div>
 
             <div class="playGrid" @click="startGame">
-                <!-- <div class="input-field" id="b1" @click="fillField('b1')"> </div> -->
-                <Cell cell-id="c1" :modelValue="cells.c1" @update:modelValue="updateCell"/>
-                <Cell cell-id="c2" :modelValue="cells.c2" @update:modelValue="updateCell"/>
-                <Cell cell-id="c3" :modelValue="cells.c3" @update:modelValue="updateCell"/>
-                <Cell cell-id="c4" :modelValue="cells.c4" @update:modelValue="updateCell"/>
-                <Cell cell-id="c5" :modelValue="cells.c5" @update:modelValue="updateCell"/>
-                <Cell cell-id="c6" :modelValue="cells.c6" @update:modelValue="updateCell"/>
-                <Cell cell-id="c7" :modelValue="cells.c7" @update:modelValue="updateCell"/>
-                <Cell cell-id="c8" :modelValue="cells.c8" @update:modelValue="updateCell"/>
-                <Cell cell-id="c9" :modelValue="cells.c9" @update:modelValue="updateCell"/>
+                <Cell v-for="(cell, key) in cells" :key="key" :cell-id="key" :modelValue="cells[key]" @update:modelValue="updateCell"/>
             </div>
         </div>
     </div>
@@ -32,27 +23,38 @@
 
 
     interface Cells { c1: string, c2: string, c3: string, c4: string, c5: string, c6: string, c7: string, c8: string, c9: string}
-    let cells: Cells = reactive({ c1: '', c2: '', c3: '', c4: '', c5: '', c6: '', c7: '', c8: '', c9: '' })
+    let cells: Cells = reactive({ c1: 'X', c2: 'O', c3: '', c4: '', c5: '', c6: '', c7: '', c8: '', c9: '' })
 
     let comment = ref<string>('');
     let isPlaying = ref<boolean>(false);
 
-    const startGame = () => !isPlaying.value? isPlaying.value =! isPlaying.value: '';
+    const startGame = () => {
+        if(!isPlaying.value) {
+            console.log('started game')
+            isPlaying.value =! isPlaying.value;
+        }
+    }
 
     const endGame = () => {
-        comment.value = 'Game Over';
-        setTimeout(() => resetGame(), 500)
+        if(process.client){
+            document
+                .querySelectorAll('.input-field')
+                .forEach(field => field.setAttribute('disabled', 'disabled'));
+        }
+
+        // setTimeout(() => resetGame(), 500)
     }
 
     const resetGame = () => {
-        isPlaying.value =! isPlaying.value;
+        isPlaying.value = false;
         for(let key in cells) cells[key] = '';
-        flag.value = 1;
+        flag.reset();
+        // if(process.client) window.location.reload();
     }
 
     const updateCell = (id: string, value: string) => {
         cells[id] = value;
-        flag.value === 1 ? comment.value = "Player X Turn": comment.value = "Player O Turn";;
+        flag.value === 1 ? comment.value = "Player X Turn": comment.value = "Player O Turn";
         checkPlayerWin(value);
     }
 
@@ -69,14 +71,7 @@
             ( cells.c3 == x && cells.c5 == x && cells.c7 == x)
         ) {
             comment.value = `Player ${x} won`;
-            if(process.client){
-                document
-                    .querySelectorAll('.input-field')
-                    .forEach(field => field.setAttribute('disabled', 'disabled'));
-                
-                setTimeout(() => endGame(), 500)
-
-            }
+            endGame();
         }else{
             checkForDraw()
         }
