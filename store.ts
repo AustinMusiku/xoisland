ccb839019649031ee417f3cad4acb0ae4d4b0455
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-05-15 03:17:07 +0300

    store reset function

--- store.ts
+++ store.ts
@@ -3,5 +3,6 @@ import { reactive } from '@nuxtjs/composition-api'
 export const flag = reactive({
   value: 1,
   increment() { this.value++ },
-  decrement() { this.value-- }
+  decrement() { this.value-- },
+  reset() { this.value = 1 }
 })
