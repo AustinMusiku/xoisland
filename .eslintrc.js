a24ba979d865dcfdac287c8e67bca2bc68ffbe14
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-05-30 20:46:36 +0300

    mod: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,15 +16,44 @@ module.exports = {
 		'vue/multi-word-component-names': [
 			'error',
 			{
-				ignores: ['index'],
+				ignores: ['index', 'Loading'],
 			},
 		],
-		// 'vue/first-attribute-linebreak': [
-		// 	'error',
-		// 	{
-		// 		singleline: 'ignore',
-		// 		multiline: 'below',
-		// 	},
-		// ],
+		'vue/first-attribute-linebreak': [
+			'error',
+			{
+				singleline: 'ignore',
+				multiline: 'below',
+			},
+		],
+		'vue/max-attributes-per-line': [
+			'error',
+			{
+				singleline: {
+					max: 1,
+				},
+				multiline: {
+					max: 1,
+				},
+			},
+		],
+		'vue/html-indent': [
+			'error',
+			'tab',
+			{
+				attribute: 1,
+				baseIndent: 1,
+				closeBracket: 0,
+				alignAttributesVertically: true,
+				ignores: [],
+			},
+		],
+		'vue/html-closing-bracket-newline': [
+			'error',
+			{
+				singleline: 'never',
+				multiline: 'always',
+			},
+		],
 	},
 }
