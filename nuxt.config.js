cc695155cb634276b5b4e93d472120c5f20dc88d
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-05-31 15:58:06 +0300

    nuxt config clean up

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,8 +54,6 @@ export default {
 		],
 	},
 
-	serverMiddleware: [{ path: '/api', handler: '~/api/index.ts' }],
-
 	// Axios module configuration: https://go.nuxtjs.dev/config-axios
 	axios: {},
 
